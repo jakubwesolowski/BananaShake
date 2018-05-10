20f9617f0ae81e2082735e14265f5a606d72e08b
dajogzum <33661462+dajogzum@users.noreply.github.com>
2018-05-10 19:12:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
 
+app.use("/public", express.static(path.join(__dirname, 'public')));//Ustawiam statyczny folder dla frontend'u
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
@@ -42,5 +45,3 @@ io.on('connection', function (socket) {
 http.listen(3000, function () {
   console.log('listening on *:3000');
 });
-
-
